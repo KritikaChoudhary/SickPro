ba2c6c550b69c7e9c694b76675be9c59c5ebcd26
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-04-12 22:34:06 +0530

    Update FirstActivity.java

--- app/src/main/java/com/example/nimishbongale/sickpro/FirstActivity.java
+++ app/src/main/java/com/example/nimishbongale/sickpro/FirstActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button b7=findViewById(R.id.button7);
         Button b1=findViewById(R.id.bu1);
+        Button rev=findViewById(R.id.button10);
 
         b7.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +30,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent=new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse("tel:108"));
                 startActivity(intent);
+            }
+        });
+        rev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String eId1="teamsickpro@gmail.com";
+                Intent email = new Intent(Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL, new String[]{eId1});
+                email.putExtra(Intent.EXTRA_SUBJECT, "Feedback:");
+                email.putExtra(Intent.EXTRA_TEXT, "Fill in the details:\n\nWhat do you like the most about our App? \n\n___\n\n Rate our App: \n\n___\n\nSuggestions/Queries: \n\n___");
+                email.setType("message/rfc822");
+                startActivity(email);
             }
         });
     }
