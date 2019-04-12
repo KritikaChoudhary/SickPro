c7c6b5ef9a92903049890847c18cc66dc4a0ad2a
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-04-12 19:14:08 +0530

    Update SecondActivityPart1.java

--- app/src/main/java/com/example/nimishbongale/sickpro/SecondActivityPart1.java
+++ app/src/main/java/com/example/nimishbongale/sickpro/SecondActivityPart1.java
@@ -1,1 +1,47 @@
+package com.example.sickpro2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        Button onl=findViewById(R.id.Onli);
+        Button offl=findViewById(R.id.Offl);
+        Button b1=findViewById(R.id.goto4);
+        final Intent implicit=new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.netmeds.com"));
+
+        onl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(implicit);
+            }
+        });
+        offl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent explicit=new Intent(MainActivity.this, Pres.class);
+                startActivity(explicit);
+            }
+        });
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent explicit=new Intent(MainActivity.this, FourthActivity.class);
+                startActivity(explicit);
+            }
+        });
+
+
+    }
+}
 
