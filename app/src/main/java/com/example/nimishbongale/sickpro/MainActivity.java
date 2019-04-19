e6ed69c94abb288cf526edddf97cd58c0bac9294
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-04-19 18:40:51 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/nimishbongale/sickpro/MainActivity.java
+++ app/src/main/java/com/example/nimishbongale/sickpro/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.nimishbongale.sickpro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        View easySplashScreenView = new EasySplashScreen(MainActivity.this)
+                .withFullScreen()
+                .withTargetActivity(FirstActivity.class)
+                .withSplashTimeOut(4500)
+                .withBackgroundResource(android.R.color.white)
+                .withHeaderText(" ")
+                .withFooterText("Copyright 2019")
+                .withBeforeLogoText("SICKPRO")
+                .withLogo(R.drawable.sickpro6)
+                .withAfterLogoText("Sick just got real.")
+                .create();
+
+        setContentView(easySplashScreenView);
     }
 }
