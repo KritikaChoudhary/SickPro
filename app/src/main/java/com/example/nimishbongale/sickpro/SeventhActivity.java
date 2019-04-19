367d2ca5ce7a67b9207d805c69c4b923605e11c8
Mahima M A <43963605+Mahima-M-A@users.noreply.github.com>
2019-04-19 18:52:13 +0530

    Update SeventhActivity.java

--- app/src/main/java/com/example/nimishbongale/sickpro/SeventhActivity.java
+++ app/src/main/java/com/example/nimishbongale/sickpro/SeventhActivity.java
@@ -1,7 +1,6 @@
 package com.example.nimishbongale.sickpro;
 
 import android.content.Intent;
-
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,35 +8,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class SeventhActivity extends AppCompatActivity {
-    Button b1, b2, b3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        b1 = findViewById(R.id.reminder);
-        b2 = findViewById(R.id.alarm);
-        b3 = findViewById(R.id.notes);
-        final Intent implicit = new Intent(Intent.ACTION_VIEW, Uri.parse("https://calendar.google.com/calendar/r"));
-        b1.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_seventh);
+        Button onl=findViewById(R.id.Onli);
+        Button offl=findViewById(R.id.Offl);
+        final Intent implicit=new Intent (Intent.ACTION_VIEW, Uri.parse("https://www.netmeds.com"));
+
+        onl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                        startActivity(implicit);
+                startActivity(implicit);
             }
         });
-                final Intent implicit1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kukuklok.com/"));
-                b2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(implicit1);
-                    }
-                });
-                final Intent implicit2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://keep.google.com/"));
-                b3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(implicit2);
-                    }
-                });
+        offl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent explicit=new Intent(SeventhActivity.this,NinthActivity.class);
+                startActivity(explicit);
             }
-        }
+        });
+    }
+}
